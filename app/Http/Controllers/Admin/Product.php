73bf68927299f7776e418913cc68025bc9952658
root <root@catalog.emshop.id>
2022-07-27 07:33:00 +0700

    img product and banner

--- app/Http/Controllers/Admin/Product.php
+++ app/Http/Controllers/Admin/Product.php
@@ -308,11 +308,15 @@ class Product extends Controller
 
     public function oracle() {
         $datas = DB::connection('oracle')->select('select prd_id,prd_nm from (SELECT m.prd_id, m.prd_nm,sum(d.ord_cost) as disp_seq from prd_prd_m m inner join ord_ord_dtl_d d on m.prd_id = d.prd_id and m.prd_ptr_cd = :a inner join ord_ord_sts_chg_h h on d.ord_id = h.ord_id and d.ord_ptr_cd in (:b) and d.ord_seq = h.ord_seq and d.ord_sts_cd = :c and h.ord_dtl_sts_cd = :d and m.prd_nm not like :e and h.ord_sts_dtm between to_date(to_char(sysdate - 7, :f), :f) and to_date(to_char(sysdate - 1, :f), :f) + 0.99999 group by m.prd_id,m.prd_nm) order by disp_seq desc',['a' => '10', 'b' => '100', 'c' => '70', 'd' => '7010', 'e' => '%NC%', 'f' => 'YYYY-MM-DD']);
-        // $item = [];
-        // foreach($datas as $key => $data) {
-        //     $item[$key] = $data->prd_id;
-        // }
-        // dd(array_slice($item, 0, 5));
-        dd($datas);
+        $item = [];
+        foreach($datas as $key => $data) {
+            //  $item[$key] = $data->prd_id;
+	   $item[$key] = DB::table('products')
+                ->select('*')
+                ->where('products.sku', $data->prd_id)
+                ->get();
+        }
+       //  dd(array_slice($item, 0, 5));
+        dd($item[0][0]);
     }
 }
